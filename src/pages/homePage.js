143ce35867b7be940b1b7c0eb14010501a2765a8
eshan1925 <eshan1925@gmail.com>
2022-01-29 02:21:58 +0530

    Added code for Landing page

---
+++ src/pages/homePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/common/Header';
+import HeroSection from '../components/HeroSection';
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+    </>
+  );
+};
+
+export default HomePage;
